b16c66332b9713c79cf1c08c1611ceeb7d34a7cf
Zeeshan Khan <zeeshu108@gmail.com>
2026-02-24 16:19:05 +0530

    Update settings.py

--- shopsphere/settings.py
+++ shopsphere/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Shopsphere',
         'USER': 'root',
-        'PASSWORD': 'Admin@123',
+        'PASSWORD': '*****',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
